f70684bbc7444f0a5842e97fa68c3a7bbaf61549
omarslm <omar.salama@gmail.com>
2024-02-26 13:22:03 -0500

    patch code

--- src/main/java/net/salama/Test.java
+++ src/main/java/net/salama/Test.java
@@ -1,11 +1,13 @@
 /**
 Comment 12
+Line 2 Test
  */
 
 package net.salama;
 public class Test{
 public static void main (String [] args){
     //------------------------
-System.out.println("Hello!!");
+    String message = "Hello!!";
+System.out.println(message);
 }
 }
